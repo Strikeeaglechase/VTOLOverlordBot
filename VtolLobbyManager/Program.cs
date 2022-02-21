929bc776f2367031e25a1ed1a6cffd3b59838634
Chase <Strikeeaglechase@gmail.com>
2022-02-21 15:21:11 -0500

    I made it work on linux

--- VtolLobbyManager/Program.cs
+++ VtolLobbyManager/Program.cs
@@ -2,20 +2,20 @@
 
 namespace VtolLobbyManager
 {
-    class Program
-    {
-        public Application app;
-        static public Program instance;
-        static public void Main(string[] args)
-        {
-            instance = new Program();
-            instance.Start();
-        }
+	class Program
+	{
+		public Application app;
+		static public Program instance;
+		static public void Main(string[] args)
+		{
+			instance = new Program();
+			instance.Start();
+		}
 
-        public void Start()
-        {
-            app = new Application();
-            app.Start().GetAwaiter().GetResult();
-        }
-    }
+		public void Start()
+		{
+			app = new Application();
+			app.Start().GetAwaiter().GetResult();
+		}
+	}
 }
