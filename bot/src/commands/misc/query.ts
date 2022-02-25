21ec6f740aa73769d21800c187df9f14ebf9d2ef
Chase <Strikeeaglechase@gmail.com>
2022-02-25 10:03:28 -0500

    Fixed query command

--- bot/src/commands/misc/query.ts
+++ bot/src/commands/misc/query.ts
@@ -12,7 +12,7 @@ class Query extends Command {
 	allowDM = false;
 	@CommandRun
 	async run({ message, framework, app }: CommandEvent<Application>, query: string) {
-		const lobby = app.lobbies.find(l => l.name.startsWith(query));
+		const lobby = app.lobbies.find(l => l.name.toLowerCase().startsWith(query.toLowerCase()));
 		if (!lobby) return framework.error(`Unable to find a lobby with the name "${query}"`);
 
 		const emb = new MessageEmbed({ title: lobby.name });
@@ -21,7 +21,7 @@ class Query extends Command {
 		emb.addField("Players", `${lobby.playerCount} of ${lobby.maxPlayers}`);
 		emb.addField("Game version", `${parseGameVersion(lobby.gameVersion)} (${lobby.gameVersion})`);
 		if (parseGameVersion(lobby.gameVersion) == "Modded") {
-			emb.addField(`Mods (${lobby.modCount})`, lobby.loadedMods.join("\n"));
+			emb.addField(`Mods (${lobby.modCount})`, lobby.loadedMods.length > 0 ? lobby.loadedMods.join("\n") : "No mods loaded (why are they even playing on modded?)");
 		}
 		return emb;
 	}
